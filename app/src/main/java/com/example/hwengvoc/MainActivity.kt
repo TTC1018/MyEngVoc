3fee1d6b92cc566b66b9e4f8b6302b14d1a7e717
TTC1018 <39405316+TTC1018@users.noreply.github.com>
2021-05-14 01:30:43 +0900

    단어 검색 Fragment 작성

--- app/src/main/java/com/example/hwengvoc/MainActivity.kt
+++ app/src/main/java/com/example/hwengvoc/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.hwengvoc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.component2
+import androidx.core.view.get
+import androidx.fragment.app.Fragment
+import com.example.hwengvoc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val dicFragment by lazy {MyDicFragment()}
+    private val searchFragment by lazy {SearchFragment()}
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
     }
+
+    private fun init() {
+        with(binding){
+            bottomNavi.setOnNavigationItemSelectedListener {
+                when(it.itemId){
+                    R.id.myDicBtn -> {
+                        changeFragment(dicFragment)
+                    }
+                    R.id.searchBtn -> {
+                        changeFragment(searchFragment)
+                    }
+                }
+                true
+            }
+            bottomNavi.selectedItemId = R.id.myDicBtn
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragContainer, fragment)
+            .commit()
+    }
+
 }
